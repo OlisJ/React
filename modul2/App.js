3de30d02ce1babedb268d97247dc39b744d33d54
OlisJ <olisj794@gmail.com>
2026-01-15 17:45:59 +0100

    .

---
+++ modul2/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import{ createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainScreen from './screens/MainScreen';
+
+const Stack = createNativeStackNavigator();
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Digi School!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
