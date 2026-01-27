446d8abb5d707a33df1ec46d6acf32647539a6e5
OlisJ <olisj794@gmail.com>
2026-01-27 16:57:19 +0100

    .

---
+++ modul2/screens/FlatListsExample.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+
+
+const FlatListsExample = () => {
+    return (
+        <View>
+            <Text>List of Students</Text>
+        </View>
+    );}
+
+export default FlatListsExample;
+            
