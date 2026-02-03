42bec2e4d8c5b0418017eddfa3edfa1899391bca
OlisJ <olisj794@gmail.com>
2026-02-03 17:38:53 +0100

    .

--- modul2/screens/ButtonScreen.js
+++ modul2/screens/ButtonScreen.js
@@ -2,14 +2,35 @@ import React from 'react';
 import { View, Text, Button, StyleSheet , TouchableOpacity } from 'react-native-web';
 
 const ButtonScreen = () => {
-    let couneter , counterT = 0;
+    let counter , counterT = 0;
     return (
         <View>
             <Text>Button Screen</Text>
-            <Button title='Click Me' color='Purple'
+            <Button title="Qlik Me" color="blue"
             onPress={() => console.log('Button Clicked!', counter++)}></Button>
+            <TouchableOpacity title="Click" style={styles.btnStyle} 
+            onPress={()=> console.log('Touchable Opacity Clicked!', counterT++)}><Text style={styles.btnText}>Touchable Opacity</Text>
+                
+            </TouchableOpacity>
             
         </View>) }
 
+const styles = StyleSheet.create({
+    textStyle: {
+        textAlign: 'center',
+        margintop: 20,
+        fontsize: 30,
+        marginbotoom: 20 },
+    btnStyle: {
+        backgroundColor: 'purple',
+        marginvertical: 10,
+        paddingVertical: 10,
+        borderRadius: 5,
+        marginHorizontal: 20 },
+    btnText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold' } });
 
 export default ButtonScreen;
