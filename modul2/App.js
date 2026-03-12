275f04e914fa0148cc1f94ebb30db44665049877
OlisJ <olisj794@gmail.com>
2026-03-12 17:24:51 +0100

    .

--- modul2/App.js
+++ modul2/App.js
@@ -1,22 +1,34 @@
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Ionicon} from '@expo/vector-icons';
 import Photos from './screens/fetchphoto';
-import Albums from './screens/fetchalbum';
-import Posts from './screens/fetchdata';
-import Users from './screens/fetchusers';
+import FlatListsExample from './screens/FlatListsExample';
+import Home from './screens/Home';
 
-const Stack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Users" screenOptions={{headerTitleAlign:'center'}}>
-        <Stack.Screen name="Users" component={Users} />
-        <Stack.Screen name="Albums" component={Albums} />
-        <Stack.Screen name="Photos" component={Photos} />
-        <Stack.Screen name="Posts" component={Posts} />
-      </Stack.Navigator>
+      <Tabs.Navigator screenoptions={({route})=>({
+        tabBarIcon:({focused,color,size})=>{
+          let iconName;
+          if (route.name === 'FlatListsExample'){
+              iconName = focused ? "flag" : "flag-outline";
+          }
+          else {
+            iconName = focused ? "newspaper" : "newspaper-outline";
+          }
+          return <Ionicon name={iconName} size={size} color={color} />;
+        }
+      })}>
+        <Tabs.Screen name="ListScreen" component={FlatListsExample} />
+        <Tabs.Screen name="Home" component={Home} />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 }
