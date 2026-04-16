76e8acbce91f8fb619341430a6622c22b6b377d5
OlisJ <olisj794@gmail.com>
2026-04-16 16:57:37 +0200

    .

---
+++ flappy/source/components/bird.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {view} from 'react-native';
+
+
+const Bird = () =>{
+    return (
+        <View style ={{
+            position:absolute,
+            backgroundColor:"Blue",
+            width:50,
+            height:50
+
+        }}></View>
+    )
+}
